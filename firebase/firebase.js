84f3c09b1158617000decc0472ceefcbfd72a793
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2025-05-10 21:43:58 -0400

    Fixed Key issue

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,6 +1,12 @@
 // firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("../key/service_account_key.json");
+const dotenv = require("dotenv");
+dotenv.config(); // Load environment variables from .env file
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY env var is missing");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
