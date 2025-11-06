d13fe5fcb426012b4b86cf26e93a68995a58e67c
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2025-11-05 22:03:29 -0500

    Added active field to model

--- models/report.js
+++ models/report.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const ReportSchema = new mongoose.Schema({
+  active: { type: Boolean, default: true },
   description: { type: String, required: true },
   location: {
     type: {
